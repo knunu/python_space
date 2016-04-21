1814cb548e4d2a962cbb7d995c9059541a9488c9
Knunu <burette@hanyang.ac.kr>
2016-04-21 18:03:34 +0900

    ID : 2854

--- Algorithm/DP/setup_problems.py
+++ Algorithm/DP/setup_problems.py
@@ -1,15 +1,40 @@
-""" Runtime Error """
-def find_case(n=0, c=0, m=0):
-    if n == 0 or c == C-1:
-        return n
+""" runtime error """
+def find_case(n, p_s, s):
+    if s:
+        if p_s:
+            if C_TT[n]: return C_TT[n]
+        else:
+            if C_FT[n]: return C_FT[n]
+        if n == C:
+            if p_s == True: return P[n] + AP[n-1]
+            else: return P[n] + AP[n-1] - 1
+        if P[n] == 0:
+            return 0
 
-    if m == 2:
-        return n * (find_case(P[c+1], c+1, 0) + find_case(HP[c]-1, c+1, 1) + find_case(HP[c+1], c+1, 2))
     else:
-        return n * (find_case(P[c+1], c+1, 0) + find_case(HP[c], c+1, 1) + find_case(HP[c+1], c+1, 2))
+        if C_F[n]: return C_F[n]
+        if n == C:
+            return P[n] + AP[n-1] - 1
+        if AP[n] == 0:
+            return 0
 
-C = int(input())
+    if s:
+        if p_s:
+            C_TT[n] = (P[n] + AP[n-1]) * (find_case(n+1, True, True) + find_case(n+1, True, False)) % 1000000007
+            return C_TT[n]
+        else:
+            C_FT[n] = (P[n] + AP[n-1] - 1) * (find_case(n+1, True, True) + find_case(n+1, True, False)) % 1000000007
+            return C_FT[n]
+    else:
+        C_F[n] = AP[n] * (find_case(n+1, False, True) + find_case(n+1, False, False)) % 1000000007
+        return C_F[n]
+
+C = int(input()) - 1
 P = [int(x) for x in input().split()]
-HP = [int(y) for y in input().split()] + [0]
+AP = [int(y) for y in input().split()] + [0]
+C_TT = [0] * (C + 1)
+C_FT = [0] * (C + 1)
+C_F = [0] * (C + 1)
 
-print(find_case(P[0], 0, 0) % 1000000007)
+print(P[0] * (find_case(1, True, True) + find_case(1, True, False)) +
+      AP[0] * (find_case(1, False, True) + find_case(1, False, False)))
